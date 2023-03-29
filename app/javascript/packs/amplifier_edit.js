8d94ebe36152d276b2c184a9d24714905fcf6b37
Daniel Torres <denial.torres@gmail.com>
2023-03-28 22:02:28 -0600

    autosave title

---
+++ app/javascript/packs/amplifier_edit.js
@@ -0,0 +1,21 @@
+import _ from 'lodash';
+
+document.addEventListener('turbo:load', () => {
+  console.log("amplifier loaded");
+  const amplifierTitleInput = document.getElementById('amplifier-title');
+  const amplifierForm = document.getElementById('amplifier-form');
+  const amplifierTitleSaved = document.getElementById('amplifier-title-saved');
+
+  if (amplifierTitleInput && amplifierForm && amplifierTitleSaved) {
+    const saveAmplifier = _.debounce(() => {
+      amplifierForm.submit();
+      amplifierTitleSaved.classList.add('show');
+
+      setTimeout(() => {
+        amplifierTitleSaved.classList.remove('show'); // Remove the "show" class after 1 second to fade out the message
+      }, 1000);
+    }, 1000);
+
+    amplifierTitleInput.addEventListener('input', saveAmplifier);
+  }
+});
