312aa81909fbba2047ce5c9c1a9d8328c0297b72
Daniel Torres <denial.torres@gmail.com>
2023-03-28 19:46:37 -0600

    Delete button using turbo

--- app/controllers/amplifiers_controller.rb
+++ app/controllers/amplifiers_controller.rb
@@ -1,5 +1,6 @@
 class AmplifiersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_amplifier, only: [:destroy]
 
   def index
     @amplifiers = current_user.amplifiers.order(created_at: :desc)
@@ -12,6 +13,31 @@ class AmplifiersController < ApplicationController
                                                 )
     @conversation = AmplifierConversation.create!(amplifier: @amplifier)
     @client = OpenAI::Client.new
+  end
+
+  def destroy
+    if @amplifier.destroy
+      # Fetch the item from the next page if available
+      pagy, _ = pagy(current_user.amplifiers.order(created_at: :desc))
+      next_item = current_user.amplifiers.order(created_at: :desc).offset(pagy.offset + pagy.items).first
+
+      # Update the current list of amplifiers
+      @pagy, @amplifiers = pagy(current_user.amplifiers.order(created_at: :desc))
+
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.remove("amplifier_#{@amplifier.id}"),
+            (turbo_stream.append("amplifiers_list", partial: "amplifier", locals: { amplifier: next_item }) if next_item),
+            turbo_stream.replace("pagination", partial: "pagination", locals: { pagy: @pagy })
+          ].compact
+        end
+        format.html { redirect_to amplifiers_path, notice: 'Amplifier successfully deleted.' }
+      end
+    else
+      flash[:alert] = "There was an error deleting the amplifier."
+      redirect_to amplifiers_path
+    end
   end
 
   def create_conversation
@@ -41,18 +67,8 @@ class AmplifiersController < ApplicationController
   def message_params
     params.require(:amplifier_conversation).permit(:content, :conversation_id)
   end
-end
 
-# render turbo_stream: [
-#   turbo_stream.replace(
-#     :conversation_form,
-#     partial: "amplifiers/form_conversation",
-#     locals: { amplifier: @amplifier, conversation: @conversation }
-#   ),
-#   turbo_stream.append(
-#     :message_container,
-#     partial: "amplifier_conversations/messages",
-#     locals: { message: @message }
-#   )
-# ]
-# end
+  def find_amplifier
+    @amplifier = current_user.amplifiers.find(params[:id])
+  end
+end
