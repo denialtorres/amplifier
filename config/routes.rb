df76892eeb9fa0d4cf2546d43f1121751c831919
denialtorres <denial.torres@gmail.com>
2023-04-14 15:14:59 -0600

    Fix rubocop stuff (#31)
    * Fix Rubocop
    
    * Fix rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
     root "amplifiers#index", as: :authenticated_root
     get 'amplifiers/new', to: 'amplifiers#new_amplifier', as: 'new_amplifier'
 
-
     resources :amplifiers do
       post 'create_conversation', on: :collection
     end
